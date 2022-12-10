81cce299317d7cf5807d5ac98afd44cce6135742
Nathaniel Ford <nathaniel.ford@gmail.com>
2022-12-10 12:12:53 -0800

    Roll back location goof

--- data_transcriber/src/data_catalogus.rs
+++ data_transcriber/src/data_catalogus.rs
@@ -1,34 +1,34 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-
-pub mod loc_from_str {
-    use serde::{de::Error as _, Deserialize, Deserializer, Serialize, Serializer};
-    use tracing::Level;
-
-    pub fn deserialize<'de, D, T>(deserializer: D) -> Result<Option<T>, D::Error>
-        where
-            D: Deserializer<'de>,
-            T: std::str::FromStr,
-            <T as std::str::FromStr>::Err: std::fmt::Display,
-    {
-        String::deserialize(deserializer)?
-            .parse::<(f64,f64)>()
-            .map(|r| Some(r))
-            .map_err(|e| D::Error::custom(format!("{}", e)))
-    }
-
-    pub fn serialize<S, T>(value: &Option<T>, serializer: S) -> Result<S::Ok, S::Error>
-        where
-            S: Serializer,
-            T: std::fmt::Display,
-    {
-        match value {
-            Some(x) => format!("{}", x),
-            None => format!("")
-        }.serialize(serializer)
-    }
-}
+//
+// pub mod loc_from_str {
+//     use serde::{de::Error as _, Deserialize, Deserializer, Serialize, Serializer};
+//     use tracing::Level;
+//
+//     pub fn deserialize<'de, D, T>(deserializer: D) -> Result<Option<T>, D::Error>
+//         where
+//             D: Deserializer<'de>,
+//             T: std::str::FromStr,
+//             <T as std::str::FromStr>::Err: std::fmt::Display,
+//     {
+//         String::deserialize(deserializer)?
+//             .parse::<(f64,f64)>()
+//             .map(|r| Some(r))
+//             .map_err(|e| D::Error::custom(format!("{}", e)))
+//     }
+//
+//     pub fn serialize<S, T>(value: &Option<T>, serializer: S) -> Result<S::Ok, S::Error>
+//         where
+//             S: Serializer,
+//             T: std::fmt::Display,
+//     {
+//         match value {
+//             Some(x) => format!("{}", x),
+//             None => format!("")
+//         }.serialize(serializer)
+//     }
+// }
 
 // First pass at catalogus
 #[derive(Debug, Serialize, Deserialize, Eq, PartialEq, JsonSchema, Clone)]
@@ -55,15 +55,16 @@ pub struct CatalogusItem {
     pub augmentum_crossbow_shared: String,
     pub augmentum_infantry: String,
     pub augmentum_infantry_shared: String,
-    #[serde(with = "loc_from_str")]
-    pub location: (f64,f64),
+    pub location: String,
+    // #[serde(with = "loc_from_str")]
+    // pub location: (f64,f64),
 }
 
 impl CatalogusItem {
     pub fn to_js(&self) -> String {
         let js = format!("{{\"loc\": [{}], \"title\":\"{}\", \"Heldfrom\":\"{}\", \"Numknights\": \"{}\", \"Aknights\": \"{}\", \"Aserg\": \"{}\",}}",
-                         // self.location,
-                         "TODO: Serialize LOC".to_string(),
+                         self.location,
+                         // "TODO: Serialize LOC".to_string(),
                          self.title,
                          self.held_from,
                          self.knights,
